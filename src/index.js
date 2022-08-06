44f2e884a804fad76678241aaaf61d1183a1771a
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-08-06 10:54:42 -0700

    Make add task button generate a task form

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 import loadPage from "./page-load";
+import taskForm from "./tasks"
 
 loadPage();
+
+function addTask() {
+    const addBtn = document.querySelector('.add-task-btn');
+
+    addBtn.addEventListener('click', taskForm);
+}
+
+addTask();
---
+++ src/tasks.js
@@ -0,0 +1,36 @@
+
+function taskForm() {
+    const container = document.querySelector('.container');
+
+    const form = document.createElement('form');
+    form.classList.add('task-form');
+    form.name = 'task-form';
+    form.textContent = 'Add New Task'
+
+    const taskDiv = document.createElement('div');
+    const taskLabel = document.createElement('label');
+    taskLabel.for = 'task';
+    const taskInput = document.createElement('input');
+    taskInput.type = 'text'; taskInput.id = 'task'; taskInput.name = 'task'; taskInput.placeholder = 'Task Description'; taskInput.required; taskInput.maxLength='40';
+    taskDiv.appendChild(taskLabel); taskDiv.appendChild(taskInput);
+
+    const prioDiv = document.createElement('div');
+    const prioSelect = document.createElement('select');
+    prioSelect.name = 'Priority';
+    const option1 = document.createElement('option');
+    option1.value = 'low';
+    option1.textContent = 'Low';
+    const option2 = document.createElement('option');
+    option2.value = 'medium';
+    option2.textContent = 'medium';
+    const option3 = document.createElement('option');
+    option3.value = 'high';
+    option3.textContent = 'High';
+    prioDiv.appendChild(prioSelect);
+    prioSelect.appendChild(option1); prioSelect.appendChild(option2); prioSelect.appendChild(option3);
+
+    form.appendChild(taskDiv); form.appendChild(prioDiv);
+    container.appendChild(form);
+}
+
+export default taskForm;
