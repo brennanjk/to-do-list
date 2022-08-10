dcce47c346db5f1c182b69a19bacf194b837ebdc
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-08-10 16:33:59 -0700

    Import new loadTask function into view-all and view-today pages and utilize in addTask functions to avoid redundant code across different modules

--- src/view-today.js
+++ src/view-today.js
@@ -1,6 +1,6 @@
 import {format} from 'date-fns';
 import clearTasks from "./clear-tasks";
-import { taskList, removeTask, toggleTask } from "./tasks";
+import { taskList, loadTask } from "./tasks";
 
 export default function loadToday() {
     // clear task DOM elements
@@ -24,39 +24,6 @@ function addTasks(tasks) {
     //write tasks to DOM from todaysTasks array
 
     tasks.forEach(task => {
-        
-        const taskDiv = document.createElement('li');
-        taskDiv.classList.add('task');
-        const taskCheck = document.createElement('input');
-        taskCheck.classList.add('task-check');
-        taskCheck.setAttribute('type','checkbox');
-        taskCheck.addEventListener('click', toggleTask);
-        const taskText = document.createElement('div');
-        taskText.textContent = `${task.description}`;
-        taskText.classList.add('description');
-        const dueDate = document.createElement('div');
-        const dateObject = new Date(task.dueDate);
-        //Adjust date object so it shows the local timezone, otherwise it will use UTC time and the returned date will be off
-        dateObject.setMinutes(dateObject.getMinutes() + dateObject.getTimezoneOffset());
-        dueDate.textContent = format(dateObject, "MM/dd/yy");
-        dueDate.classList.add('due-date');
-        const taskPriority = document.createElement('div');
-        taskPriority.textContent = `${task.priority}`;
-        taskPriority.classList.add(`${task.priority}`);
-        taskPriority.classList.add('priority');
-        const taskDelete = document.createElement('button');
-        taskDelete.classList.add('task-delete');
-        taskDelete.textContent = 'X';
-        taskDelete.addEventListener('click', removeTask);
-        //append objects to task li element
-        taskDiv.appendChild(taskCheck);
-        taskDiv.appendChild(taskText);
-        taskDiv.appendChild(dueDate);
-        taskDiv.appendChild(taskPriority);
-        taskDiv.appendChild(taskDelete);
-
-        //append new task to the taskList
-        const taskContainer = document.querySelector('.task-list');
-        taskContainer.appendChild(taskDiv);
+        loadTask(task);
     })
 }
