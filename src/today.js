7127ac58e4d868e47570c163081bd142bdf98881
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-08-10 13:48:06 -0700

    Make tasksToday create a new array that contains all tasks from the root taskList with a duedate that matches todays date

--- src/today.js
+++ src/today.js
@@ -1,11 +1,19 @@
+import {format} from 'date-fns';
 import clearTasks from "./clear-tasks";
-import taskList from "./tasks";
+import { taskList } from "./tasks";
 
 export default function tasksToday() {
+    // clear task DOM elements
     clearTasks();
-    /*const todaysDate = new Date();
+    
+    //create variable that will always be the current date when the function runs.
+    const todaysDate = new Date();
+    const formattedDate = format(todaysDate, "yyyy-MM-dd")
+    console.log(formattedDate)
+    console.log(taskList[0].dueDate)
 
-    taskList.forEach(task => {
-        
-    });*/
+    const todaysTasks = taskList.filter(task => task.dueDate === formattedDate);
+
+    return {todaysTasks}
 }
+
