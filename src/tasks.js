c193a9a3236c1b9ca34db80bbc21b3b61e06382c
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-08-10 16:25:20 -0700

    Nest several functions related to adding a function to the DOM into one root function
    addTask, toggleTask, & removeTask all tested into a loadTask function. Successfully tested from root page

--- src/tasks.js
+++ src/tasks.js
@@ -2,7 +2,12 @@ import {addDays, format} from 'date-fns';
 
 export const taskList = [];
 
-export {removeTask, toggleTask};
+function addToList(newTask) {
+    //add task to taskList array
+    taskList.push(newTask);
+}
+
+export {loadTask};
 
 export default function taskForm() {
     const container = document.querySelector('.container');
@@ -57,71 +62,73 @@ class task{
     };
 }
 
-function addTask(newTask) {
-    //add task to taskList array
-    taskList.push(newTask);
+function loadTask(task) {
 
-    //add task to task list regardless of current view
-    const task = document.createElement('li');
-    task.classList.add('task');
-    const taskCheck = document.createElement('input');
-    taskCheck.classList.add('task-check');
-    taskCheck.setAttribute('type','checkbox');
-    taskCheck.addEventListener('click', toggleTask);
-    const taskText = document.createElement('div');
-    taskText.textContent = `${newTask.description}`;
-    taskText.classList.add('description');
-    const dueDate = document.createElement('div');
-    const dateObject = new Date(newTask.dueDate);
-    //Adjust date object so it shows the local timezone, otherwise it will use UTC time and the returned date will be off
-    dateObject.setMinutes(dateObject.getMinutes() + dateObject.getTimezoneOffset());
-    dueDate.textContent = format(dateObject, "MM/dd/yy");
-    dueDate.classList.add('due-date');
-    const taskPriority = document.createElement('div');
-    taskPriority.textContent = `${newTask.priority}`;
-    taskPriority.classList.add(`${newTask.priority}`);
-    taskPriority.classList.add('priority');
-    const taskDelete = document.createElement('button');
-    taskDelete.classList.add('task-delete');
-    taskDelete.textContent = 'X';
-    taskDelete.addEventListener('click', removeTask);
-    //append objects to task li element
-    task.appendChild(taskCheck);
-    task.appendChild(taskText);
-    task.appendChild(dueDate);
-    task.appendChild(taskPriority);
-    task.appendChild(taskDelete);
+    function addTask(newTask) {
+        //Add task to DOM
+        const task = document.createElement('li');
+        task.classList.add('task');
+        const taskCheck = document.createElement('input');
+        taskCheck.classList.add('task-check');
+        taskCheck.setAttribute('type','checkbox');
+        taskCheck.addEventListener('click', toggleTask);
+        const taskText = document.createElement('div');
+        taskText.textContent = `${newTask.description}`;
+        taskText.classList.add('description');
+        const dueDate = document.createElement('div');
+        const dateObject = new Date(newTask.dueDate);
+        //Adjust date object so it shows the local timezone, otherwise it will use UTC time and the returned date will be off
+        dateObject.setMinutes(dateObject.getMinutes() + dateObject.getTimezoneOffset());
+        dueDate.textContent = format(dateObject, "MM/dd/yy");
+        dueDate.classList.add('due-date');
+        const taskPriority = document.createElement('div');
+        taskPriority.textContent = `${newTask.priority}`;
+        taskPriority.classList.add(`${newTask.priority}`);
+        taskPriority.classList.add('priority');
+        const taskDelete = document.createElement('button');
+        taskDelete.classList.add('task-delete');
+        taskDelete.textContent = 'X';
+        taskDelete.addEventListener('click', removeTask);
+        //append objects to task li element
+        task.appendChild(taskCheck);
+        task.appendChild(taskText);
+        task.appendChild(dueDate);
+        task.appendChild(taskPriority);
+        task.appendChild(taskDelete);
 
-    //append new task to the taskList
-    const tasks = document.querySelector('.task-list');
-    tasks.appendChild(task);
-}
-
-function removeTask() {
-    //Create variable for the sibling task description div related to the delete button pressed
-    const descriptor = this.parentNode.querySelector('.description');
-    console.log(descriptor.textContent);
-
-    //Find task in taskList array and remove it
-    const taskIndex = taskList.findIndex(task => task.description === descriptor.textContent);
-    taskList.splice(taskIndex, 1);
-
-    //remove the HTML element from the DOM
-    this.parentNode.remove();
-}
-
-function toggleTask() {
-    //Create variable for the parent element of the checked box
-    const checkParent = this.parentNode;
-
-    if (this.checked) {
-        checkParent.style.textDecoration = 'line-through';
-        checkParent.classList.add('completed');
+        //append new task to the taskList
+        const tasks = document.querySelector('.task-list');
+        tasks.appendChild(task);
     }
-    else {
-        checkParent.style.textDecoration = 'none';
-        checkParent.classList.remove('completed');
+
+    function removeTask() {
+        //Create variable for the sibling task description div related to the delete button pressed
+        const descriptor = this.parentNode.querySelector('.description');
+        console.log(descriptor.textContent);
+
+        //Find task in taskList array and remove it
+        const taskIndex = taskList.findIndex(task => task.description === descriptor.textContent);
+        taskList.splice(taskIndex, 1);
+
+        //remove the HTML element from the DOM
+        this.parentNode.remove();
     }
+
+    function toggleTask() {
+        //Create variable for the parent element of the checked box
+        const checkParent = this.parentNode;
+
+        if (this.checked) {
+            checkParent.style.textDecoration = 'line-through';
+            checkParent.classList.add('completed');
+        }
+        else {
+            checkParent.style.textDecoration = 'none';
+            checkParent.classList.remove('completed');
+        }
+    }
+
+    addTask(task);
 }
 
 function submitTask(event) {
@@ -130,7 +137,8 @@ function submitTask(event) {
     const dueDate = document.querySelector('[name="due-date"]').value;
     const newTask = new task(description, priority, dueDate);
 
-    addTask(newTask);
+    addToList(newTask);
+    loadTask(newTask);
     console.log(taskList);
     event.preventDefault();
     this.reset();
