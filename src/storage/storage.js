f175989ba51742b84ff273a3e4bae8467202b1ed
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-08-12 14:53:47 -0700

    Add JSON stringify and parsing to storeTaskList - confirm console.log returns an array of tasks now

--- src/storage/storage.js
+++ src/storage/storage.js
@@ -3,7 +3,7 @@ import { taskList } from '../task-objects'
 export {storeTaskList};
 
 function storeTaskList() {
-    sessionStorage.setItem('taskList',taskList);
-    const sessionTaskList = sessionStorage.getItem("taskList");
+    sessionStorage.setItem('taskList', JSON.stringify(taskList));
+    const sessionTaskList = JSON.parse(sessionStorage.getItem("taskList"));
     console.log(sessionTaskList);
 }
