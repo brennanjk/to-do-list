554dd5b992144c46fd1d0f0ff98491a7dec5e15c
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-08-13 11:54:57 -0700

    Add project image icon and adjust project element structure accordingly

--- src/projects/new-project.js
+++ src/projects/new-project.js
@@ -52,8 +52,14 @@ function loadProject(newProject) {
 
     const project = document.createElement('li');
     project.classList.add('project');
-    project.textContent = newProject.name;
+    const projectImage = document.createElement('img');
+    projectImage.src = '../dist/images/blueprint.svg';
+    projectImage.alt = 'project icon';
+    const projectName = document.createElement('div');
+    projectName.textContent = newProject.name;
 
     //append elements to Dom
+    project.appendChild(projectImage);
+    project.appendChild(projectName);
     projectNavList.appendChild(project);
 }
