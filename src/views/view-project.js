ba2397774b5364aef310c0b7412c19552438b383
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-08-15 14:15:11 -0700

    Make clicking a project load any existing project tasks in the DOM

--- src/views/view-project.js
+++ src/views/view-project.js
@@ -1,9 +1,24 @@
 import clearTasks from "../tasks/clear-tasks";
+import { loadTask } from "../tasks/task-functions";
 import { projectTaskButton } from "../projects/project-tasks";
 import { activeProject } from "../projects/project-objects";
+import { projectList } from "../projects/project-objects";
 
 export default function loadProjectView() {
     clearTasks();
     projectTaskButton();
     activeProject();
+    addProjectTasks();
+}
+
+function addProjectTasks() {
+    //find active project
+    const activeProject = projectList.find(project => project.active === true);
+
+    function loadTasks(tasks) {
+        tasks.forEach(task => {
+            loadTask(task);
+        });
+    }
+    loadTasks(activeProject.projectTasks);
 }
