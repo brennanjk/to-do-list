fa50ab355fd337abb9e13f939d863c9132ff3aa4
Brennan Kilpatrick <brennankilpatrick@gmail.com>
2022-08-15 12:01:19 -0700

    Make clicking a project reset all other projects active value to false, then set the clicked project active value to true

--- src/projects/project-objects.js
+++ src/projects/project-objects.js
@@ -14,8 +14,17 @@ class project{
 }
 
 function activeProject() {
-    console.log(event.target);
+    //ensure no other projects are set as active
+    projectList.forEach(project => project.active = false);
 
-    //const projectIndex = projectList.findIndex(project => project.name === this.textContent);
-    //console.log(projectIndex)
+    //create a variable for the name of the clicked project
+    const projectName = event.target.innerText;
+
+    //find the index of the project in the project list
+    const projectIndex = projectList.findIndex(project => project.name === projectName);
+    console.log(projectIndex)
+
+    //set the project at the index 'active' key to true
+    projectList[projectIndex].active = true;
+    console.log(projectList[projectIndex]);
 }
